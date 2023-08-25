bf7d68a0bf2fafbc79d7cea13cd45c32b230ff6d
Ofir Levi <ofirl24@gmail.com>
2023-08-26 00:06:05 +0300

    fix

--- management-server/consts.py
+++ management-server/consts.py
@@ -1,5 +1,5 @@
 import os
 
 SOURCE_DIR = os.path.dirname(__file__)
-db_file = os.path.join(SOURCE_DIR, "/../configs.db")
+db_file = os.path.join(SOURCE_DIR, "../configs.db")
 words_directory = os.path.join(SOURCE_DIR, "words")
