60b9c7e56ac1fca774a80a980145390b6030a273
Ofir Levi <ofirl24@gmail.com>
2023-08-25 22:54:09 +0300

    package json

--- management-console/src/api/configs.tsx
+++ management-console/src/api/configs.tsx
@@ -1,6 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
+const baseUrl = "http://speakstick.local"
+
 export const useGetConfigs = () => {
-    return useQuery(['configs'], () => axios.get("/api/configs"))
+    return useQuery(['configs'], () => axios.get(baseUrl + "/api/configs"))
 };
