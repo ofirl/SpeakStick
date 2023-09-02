c853fa29be833bf174a1ee9344a0a203aaf08489
Ofir Levi <ofirl24@gmail.com>
2023-09-02 10:05:16 +0300

    fix

--- management-server/handlers/positions_handlers.py
+++ management-server/handlers/positions_handlers.py
@@ -1,9 +1,9 @@
-import db
+import db_utils
 
 import response_utils
 
 def getPositions(self):
-    positions = db.get_positions()
+    positions = db_utils.get_positions()
     if positions:
         response_utils.writeJsonResponse(self, positions)
     else:
