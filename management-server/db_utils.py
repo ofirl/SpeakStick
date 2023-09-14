e1c46054a9b4d1df040c7e0690ffdea9412d12b6
Ofir Levi <ofirl24@gmail.com>
2023-09-15 01:03:39 +0300

    test

--- management-server/db_utils.py
+++ management-server/db_utils.py
@@ -58,7 +58,7 @@ def get_library_items(libraryId):
         if libraryId == None:
             cursor.execute("SELECT * FROM library_items")
         else:
-            cursor.execute("SELECT * FROM library_items WHERE libraryId = ?", (int("".join(libraryId)),))
+            cursor.execute("SELECT * FROM library_items WHERE libraryId = ?", ("".join(libraryId),))
         
         # Fetch all the rows of data
         rows = cursor.fetchall()
