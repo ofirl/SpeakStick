55e47c47881ec65cf8147eece03ad31d78644ae6
Ofir Levi <ofirl24@gmail.com>
2023-09-15 00:13:15 +0300

    fix

--- stick-controller/words.py
+++ stick-controller/words.py
@@ -13,7 +13,7 @@ def get_word_by_position(position):
         cursor = connection.cursor()
         
         # Execute a query to retrieve the row with the specified position
-        cursor.execute("SELECT * FROM words WHERE positions = ?", (position,))
+        cursor.execute("SELECT * FROM library_items WHERE libraryId in (SELECT id FROM libraries WHERE active = True) AND positions = ?", (position,))
         
         # Fetch the row data
         row = cursor.fetchone()
