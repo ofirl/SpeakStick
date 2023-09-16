e88acbbe9bcd0fbddc1b31f3780229712454cd3f
Ofir Levi <ofirl24@gmail.com>
2023-09-16 23:42:05 +0300

    test

--- management-server/system_utils.py
+++ management-server/system_utils.py
@@ -260,8 +260,7 @@ def get_versions():
     try:
         repo = git.Repo("/opt/SpeakStick")
         tags = [str(tag) for tag in repo.tags]
-        print(tags)
-        return tags.sort(reverse=True)
+        return sorted(tags, reverse=True)
 
     except Exception as e:
         print(f"An error occurred: {e}")
