e4826cc4a635537ba613517154e82da06942e969
Ofir Levi <ofirl24@gmail.com>
2023-09-16 23:00:30 +0300

    version 3

--- management-console/src/screens/AdvancedSettings/AdvancedSettings.tsx
+++ management-console/src/screens/AdvancedSettings/AdvancedSettings.tsx
@@ -1,7 +1,9 @@
 import { Autocomplete, TextField } from "@mui/material";
 import { useApplicationVersions } from "../../api/system";
+import { useState } from "react";
 
 export const AdvancedSettings = () => {
+    const [developmentBuildsEnabled, setDevelopmentBuildsEnabled] = useState(false)
     const { data: versions } = useApplicationVersions();
     const currentVersion = "0.1";
     // const versions = ["0.1", "0.2"]
