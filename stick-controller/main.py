3475580d35be9698d0275e35c93dcf3c9ea949da
Ofir Levi <ofirl24@gmail.com>
2023-10-01 22:17:56 +0300

    test

--- stick-controller/main.py
+++ stick-controller/main.py
@@ -8,6 +8,22 @@ import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
 from pygame import mixer
 
+import pygame._sdl2.audio as sdl2_audio
+
+
+def get_devices(capture_devices: bool = False) -> Tuple[str, ...]:
+    init_by_me = not pygame.mixer.get_init()
+    if init_by_me:
+        pygame.mixer.init()
+    devices = tuple(sdl2_audio.get_audio_device_names(capture_devices))
+    if init_by_me:
+        pygame.mixer.quit()
+    return devices
+
+
+devices = get_devices()
+print(devices)
+
 import db_default
 
 from config import configs
