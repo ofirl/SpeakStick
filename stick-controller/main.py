23164c06f804e21f984d3d3f56c885113412b557
Ofir Levi <ofirl24@gmail.com>
2023-10-01 22:19:47 +0300

    test

--- stick-controller/main.py
+++ stick-controller/main.py
@@ -9,14 +9,13 @@ from adafruit_mcp3xxx.analog_in import AnalogIn
 from pygame import mixer
 
 import pygame
-import pygame._sdl2.audio as sdl2_audio
 
 
 def get_devices(capture_devices: bool = False) -> Tuple[str, ...]:
     init_by_me = not pygame.mixer.get_init()
     if init_by_me:
         pygame.mixer.init()
-    devices = tuple(sdl2_audio.get_audio_device_names(capture_devices))
+    devices = tuple(pygame._sdl2_audio.get_audio_device_names(capture_devices))
     if init_by_me:
         pygame.mixer.quit()
     return devices
