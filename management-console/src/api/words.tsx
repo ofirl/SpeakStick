112910e5bd38b027b12b89ebaa4fb7f5e3cca395
Ofir Levi <ofirl24@gmail.com>
2023-11-17 07:33:34 +0200

    support all languages in file names

--- management-console/src/api/words.tsx
+++ management-console/src/api/words.tsx
@@ -5,43 +5,43 @@ import { baseUrl } from "./consts";
 import { toast } from "react-toastify";
 
 export const useGetWords = () => {
-    return useQuery(['words'], () => axios.get(baseUrl + "/words").then(value => value.data as string[]))
+  return useQuery(['words'], () => axios.get(baseUrl + "/words").then(value => (value.data as string[]).map(w => decodeURIComponent(w))))
 };
 
 type DeleteWordParams = { word: string };
 export const useDeleteWord = () => {
-    const queryClient = useQueryClient();
-    return useMutation((params: DeleteWordParams) =>
-        axios.delete(baseUrl + "/words", { params }).then(value => value.status === 200),
-        {
-            onSuccess: () => {
-                queryClient.invalidateQueries(["words"]);
-                toast.success("Word Deleted")
-            },
-            onError: () => {
-                toast.error("Error deleting word")
-            }
-        }
-    )
+  const queryClient = useQueryClient();
+  return useMutation(({ word }: DeleteWordParams) =>
+    axios.delete(baseUrl + "/words", { params: { word: encodeURIComponent(word) } }).then(value => value.status === 200),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(["words"]);
+        toast.success("Word Deleted")
+      },
+      onError: () => {
+        toast.error("Error deleting word")
+      }
+    }
+  )
 };
 
 type UploadWordParams = { file: File, fileName: string };
 export const useUploadWord = () => {
-    const queryClient = useQueryClient();
-    return useMutation(({ file, fileName }: UploadWordParams) =>
-        axios.post(baseUrl + "/words", file, {
-            headers: {
-                filename: fileName
-            }
-        }).then(value => value.status === 200),
-        {
-            onSuccess: () => {
-                queryClient.invalidateQueries(["words"]);
-                toast.success("Word Uploaded")
-            },
-            onError: () => {
-                toast.error("Error uploading word")
-            }
-        }
-    )
+  const queryClient = useQueryClient();
+  return useMutation(({ file, fileName }: UploadWordParams) =>
+    axios.post(baseUrl + "/words", file, {
+      headers: {
+        filename: encodeURIComponent(fileName)
+      }
+    }).then(value => value.status === 200),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(["words"]);
+        toast.success("Word Uploaded")
+      },
+      onError: () => {
+        toast.error("Error uploading word")
+      }
+    }
+  )
 };
--- management-server/utils/system_utils.py
+++ management-server/utils/system_utils.py
@@ -102,7 +102,7 @@ def is_process_running(process_name):
 def get_sound_cards():
     try:
         # Run the 'aplay -l' command and capture its output
-        output = subprocess.check_output(['aplay', '-l'], universal_newlines=True)
+        output = subprocess.check_output(["aplay", "-l"], universal_newlines=True)
 
         # Define a regular expression pattern to match lines with USB Audio devices
         pattern = r"card (\d+): (.*) \[.*\], device \d+: (.*) \[.*\]"
@@ -117,10 +117,11 @@ def get_sound_cards():
         print(f"Error: {e}")
         return None
 
+
 def write_default_sound_config(card_number):
     try:
         # Open the file in write mode ('w')
-        with open("/etc/asound.conf", 'w') as file:
+        with open("/etc/asound.conf", "w") as file:
             # Write the lines to the file
             file.write(f"defaults.pcm.card {card_number}\n")
             file.write(f"defaults.ctl.card {card_number}\n")
@@ -136,6 +137,9 @@ def write_default_sound_config(card_number):
 
 def get_usb_sound_card():
     cards = get_sound_cards()
+    if cards is None:
+        print("Error getting sound cards")
+        return None
 
     for card_number, card_name, device_name in cards:
         if "USB Audio" in device_name:
@@ -144,11 +148,12 @@ def get_usb_sound_card():
     # If no USB Audio device is found, return None
     return None
 
+
 def set_default_audio_output():
     try:
         card_number = get_usb_sound_card()
         if card_number is None:
-            print("No USB sound card found") 
+            print("No USB sound card found")
             return
 
         write_default_sound_config(card_number)
