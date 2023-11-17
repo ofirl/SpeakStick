43265e2116ee9d1b04e77f9e0da0a42409242594
Ofir Levi <ofirl24@gmail.com>
2023-11-17 08:54:54 +0200

    basic log viewer

--- management-console/src/screens/Logs/Logs.tsx
+++ management-console/src/screens/Logs/Logs.tsx
@@ -5,24 +5,29 @@ import CircularProgress from "@mui/material/CircularProgress";
 import TextField from "@mui/material/TextField";
 import { Services, servicesOptions, useGetServiceLogs } from "../../api/system";
 
+type LogViewerProps = {
+  logs?: string
+}
+const LogViewer = ({ logs = "" }: LogViewerProps) => {
+  return <textarea value={logs} style={{ width: "100%", height: "100%" }} />
+}
+
 export const Logs = () => {
   const [selectedService, setSelectedService] = useState<Services>(servicesOptions[0])
   const { data: serviceLogs, isLoading: isLoadingServiceLogs } = useGetServiceLogs(selectedService)
 
   return (
     <div style={{ maxWidth: "50rem", gap: "1rem", display: "flex", flexDirection: "column", height: "100%", flexGrow: 1 }}>
-      <div>
-        <Autocomplete
-          style={{ flexGrow: "1" }}
-          value={selectedService}
-          options={servicesOptions}
-          renderInput={(params) => <TextField {...params} label="Service" />}
-          onChange={(_e, value) => setSelectedService(value as Services)}
-          disableClearable
-          blurOnSelect
-        />
-        {isLoadingServiceLogs ? <CircularProgress /> : serviceLogs}
-      </div>
+      <Autocomplete
+        style={{ flexGrow: "1" }}
+        value={selectedService}
+        options={servicesOptions}
+        renderInput={(params) => <TextField {...params} label="Service" />}
+        onChange={(_e, value) => setSelectedService(value as Services)}
+        disableClearable
+        blurOnSelect
+      />
+      {isLoadingServiceLogs ? <CircularProgress /> : <LogViewer logs={serviceLogs} />}
     </div>
   )
 };
