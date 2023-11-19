2eaeae19dff7ca353c1eb3f632decadd31c83007
Ofir Levi <ofirl24@gmail.com>
2023-11-19 21:34:49 +0200

    fix bug

--- management-console/src/screens/library/Library.tsx
+++ management-console/src/screens/library/Library.tsx
@@ -21,82 +21,82 @@ import { LibraryControls } from './LibraryControls';
 import { Divider } from '@mui/material';
 
 export const Library = () => {
-    const [filter, setFilter] = useState("");
-    const [selectedLibrary, setSelectedLibrary] = useState<LibraryT | undefined>();
+  const [filter, setFilter] = useState("");
+  const [selectedLibrary, setSelectedLibrary] = useState<LibraryT | undefined>();
 
-    const { data: libraryItems = [], isLoading } = useGetLibraryItems(selectedLibrary?.id);
+  const { data: libraryItems = [], isLoading } = useGetLibraryItems(selectedLibrary?.id || 1);
 
-    const onFilterChange = useCallback((value: string) => {
-        setFilter(value.toLowerCase())
-    }, [])
-    const onFilterChangeDebounced = useDebounce(onFilterChange, 200)
+  const onFilterChange = useCallback((value: string) => {
+    setFilter(value.toLowerCase())
+  }, [])
+  const onFilterChangeDebounced = useDebounce(onFilterChange, 200)
 
-    const { mutateAsync: deletePosition, isLoading: isDeleting } = useDeleteLibraryItem();
+  const { mutateAsync: deletePosition, isLoading: isDeleting } = useDeleteLibraryItem();
 
-    return (
-        <div style={{ maxWidth: "50rem", gap: "1rem", display: "flex", flexDirection: "column", height: "100%", flexGrow: 1 }}>
-            <LibraryControls selectedLibrary={selectedLibrary} onChange={setSelectedLibrary} />
-            <Divider />
-            <div style={{ display: "flex", gap: "0.5rem" }}>
-                <Autocomplete
-                    style={{ flexGrow: "1" }}
-                    freeSolo
-                    options={Array.from(new Set(libraryItems.flatMap(i => [i.positions.toString(), i.word])))}
-                    renderInput={(params) => <TextField {...params} label="Filter words" />}
-                    onInputChange={(_e, value) => onFilterChangeDebounced(value || "")}
-                />
-                <AddWordModal disabled={!selectedLibrary?.editable} libraryId={selectedLibrary?.id || 1} />
-            </div>
-            <TableContainer component={Paper}>
-                <Table aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell sx={(theme) => ({ width: window.innerWidth < theme.breakpoints.values.sm ? "4rem" : "6rem", background: "white" })}> Positions </TableCell>
-                            <TableCell sx={{ background: "white" }}> Word </TableCell>
-                            <TableCell sx={{ width: "3rem", background: "white" }} />
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {isLoading ?
-                            [1, 2].map(i => (
-                                <TableRow
-                                    key={"row" + i}
-                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                >
-                                    <TableCell colSpan={3} component="th" scope="row">
-                                        <Skeleton variant="rounded" height={"2rem"} />
-                                    </TableCell>
-                                </TableRow>
-                            ))
-                            :
-                            libraryItems.filter(({ positions, word }) =>
-                                positions.toString().toLowerCase().includes(filter) ||
-                                word.toLowerCase().includes(filter))
-                                .map(({ positions, word }) => (
-                                    <TableRow
-                                        key={positions}
-                                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                    >
-                                        <TableCell component="th" scope="row"> {positions} </TableCell>
-                                        <TableCell> {word} </TableCell>
-                                        <TableCell>
-                                            <IconButton
-                                                disabled={isDeleting || !selectedLibrary || !selectedLibrary.editable}
-                                                size="large"
-                                                color="inherit"
-                                                aria-label="delete word"
-                                                onClick={() => selectedLibrary && deletePosition({ libraryId: selectedLibrary.id, position: positions.toString() })}
-                                            >
-                                                {isDeleting ? <CircularProgress /> : <DeleteIcon />}
-                                            </IconButton>
-                                        </TableCell>
-                                    </TableRow>
-                                ))
-                        }
-                    </TableBody>
-                </Table>
+  return (
+    <div style={{ maxWidth: "50rem", gap: "1rem", display: "flex", flexDirection: "column", height: "100%", flexGrow: 1 }}>
+      <LibraryControls selectedLibrary={selectedLibrary} onChange={setSelectedLibrary} />
+      <Divider />
+      <div style={{ display: "flex", gap: "0.5rem" }}>
+        <Autocomplete
+          style={{ flexGrow: "1" }}
+          freeSolo
+          options={Array.from(new Set(libraryItems.flatMap(i => [i.positions.toString(), i.word])))}
+          renderInput={(params) => <TextField {...params} label="Filter words" />}
+          onInputChange={(_e, value) => onFilterChangeDebounced(value || "")}
+        />
+        <AddWordModal disabled={!selectedLibrary?.editable} libraryId={selectedLibrary?.id || 1} />
+      </div>
+      <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell sx={(theme) => ({ width: window.innerWidth < theme.breakpoints.values.sm ? "4rem" : "6rem", background: "white" })}> Positions </TableCell>
+              <TableCell sx={{ background: "white" }}> Word </TableCell>
+              <TableCell sx={{ width: "3rem", background: "white" }} />
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {isLoading ?
+              [1, 2].map(i => (
+                <TableRow
+                  key={"row" + i}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell colSpan={3} component="th" scope="row">
+                    <Skeleton variant="rounded" height={"2rem"} />
+                  </TableCell>
+                </TableRow>
+              ))
+              :
+              libraryItems.filter(({ positions, word }) =>
+                positions.toString().toLowerCase().includes(filter) ||
+                word.toLowerCase().includes(filter))
+                .map(({ positions, word }) => (
+                  <TableRow
+                    key={positions}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row"> {positions} </TableCell>
+                    <TableCell> {word} </TableCell>
+                    <TableCell>
+                      <IconButton
+                        disabled={isDeleting || !selectedLibrary || !selectedLibrary.editable}
+                        size="large"
+                        color="inherit"
+                        aria-label="delete word"
+                        onClick={() => selectedLibrary && deletePosition({ libraryId: selectedLibrary.id, position: positions.toString() })}
+                      >
+                        {isDeleting ? <CircularProgress /> : <DeleteIcon />}
+                      </IconButton>
+                    </TableCell>
+                  </TableRow>
+                ))
+            }
+          </TableBody>
+        </Table>
 
-            </TableContainer>
-        </div>
-    )
+      </TableContainer>
+    </div>
+  )
 };
