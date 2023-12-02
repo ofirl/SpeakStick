e2b09745f4337410c1c8d1a4b4f7f54ad1c63d54
Ofir Levi <ofirl24@gmail.com>
2023-12-02 11:17:14 +0200

    fix

--- init/init.sh
+++ init/init.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-# NEW_RELIC_API_KEY=NRAK-7L1RWP00BHYDQJZ7BHMLE08MV1C NEW_RELIC_ACCOUNT_ID=4264684 NEW_RELIC_REGION=EU
-
 # admin password: SpeakStick4U!
 
 # Run this script after cloning the repository:
