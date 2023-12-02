591a1b1bce7dd5683c36ac29f0449efea8e8a73e
Ofir Levi <ofirl24@gmail.com>
2023-12-02 21:13:01 +0200

    test

--- management-server/monitoring/log_configs.py
+++ management-server/monitoring/log_configs.py
@@ -3,18 +3,26 @@ import time
 import logging
 import logging.handlers
 
-log_handler = logging.handlers.WatchedFileHandler("/opt/logs/management-server.log")
 # create formatter
 formatter = logging.Formatter(
     "%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s",
     "%Y-%m-%d %H:%M:%S",
 )
+
+# console handler
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setLevel(logging.DEBUG)
+console_handler.setFormatter(formatter)
+
+# file handler
+file_handler = logging.handlers.WatchedFileHandler("/opt/logs/management-server.log")
 # formatter.converter = time.gmtime  # if you want UTC time
-log_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
 
 logger = logging.getLogger()
-logger.addHandler(log_handler)
+logger.addHandler(file_handler)
 logger.setLevel(logging.DEBUG)
+
 
 # 'application' code
 logger.debug("debug message")
