9b7879429e9698d66ac304a690e66c7b8ee51cac
Ofir Levi <ofirl24@gmail.com>
2023-12-07 23:06:21 +0200

    test

--- upgrade-script.sh
+++ upgrade-script.sh
@@ -5,6 +5,7 @@ latest_tag=$1
 workdir="/opt/SpeakStick"
 cd "${workdir}"
 
+echo "Fetching repo"
 git fetch
 
 if [ -z "$latest_tag" ]
@@ -17,6 +18,7 @@ then
         exit 0
     fi
 
+    echo "Get all tags"
     # get all tags and filter development build if needed
     development_builds=$(sqlite3 ./configs.db "select value from configs where key='ENABLE_DEVELOPMENT_BUILDS'")
     if [ $? != 0 ] || [ $development_builds != "1" ]
@@ -33,25 +35,35 @@ echo "Upgrading from $current_version to $latest_tag"
 
 git checkout $latest_tag
 
+echo "Installing requirements"
 pip3 install -r requirements.txt
 
+echo "Downloading artifacts"
 # Download console-management artifact
 wget -P /tmp https://github.com/ofirl/SpeakStick/releases/download/$latest_tag/management-console.zip
+
 # Decompress console-management artifact
+echo "Decompressing artifacts"
 rm -r /opt/SpeakStick/management-console/dist
 unzip /tmp/management-console.zip -d /opt/SpeakStick/management-console
+
 # Clean up
+echo "Cleaning up artifacts"
 rm /tmp/management-console.zip
 
 # Run migrations
+echo "Running migrations"
 /usr/bin/python3 -u /opt/SpeakStick/migrations/migrate.py ${current_version//v/} ${latest_tag//v/}
 
 # update nginx.conf
+echo "Updating nginx conf"
 sudo cp /opt/SpeakStick/init/nginx.conf /etc/nginx/sites-enabled/default
-# reload nginx
+# reload
 sudo /etc/init.d/nginx reload
 
+echo "Updating systemctl services"
 sudo ./init/init_systemctl_services.sh
 
 # always restart the services last
+echo "Restarting services"
 sudo systemctl restart speakstick speakstick-management-server
