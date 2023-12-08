a7c8c6b40bceaeb076babd0944f78acf368e52ed
Ofir Levi <ofirl24@gmail.com>
2023-12-08 19:28:27 +0200

    test

--- monitoring/logs_handler.py
+++ monitoring/logs_handler.py
@@ -185,20 +185,37 @@ def extract_timestamp_from_nginx_log(log_line):
         return None
 
 
+def parse_nginx_log(log_line):
+    log_pattern = re.compile(
+        r'(?P<ip>[\w.:]+) - - \[(?P<timestamp>[^\]]+)\] "(?P<method>[A-Z]+) (?P<path>[^"]+)" (?P<status>\d+) (?P<size>\d+) "(?P<referrer>[^"]+)" "(?P<user_agent>[^"]+)"'
+    )
+
+    match = log_pattern.match(log_line)
+    if match:
+        log_data = match.groupdict()
+
+        # Convert timestamp to ISO format
+        log_data["timestamp"] = datetime.strptime(
+            log_data["timestamp"], "%d/%b/%Y:%H:%M:%S %z"
+        ).timestamp()
+
+        # Convert size to integer
+        log_data["size"] = int(log_data["size"])
+
+        log_data["message"] = log_line
+        return log_data
+    else:
+        return None
+
+
 def format_log(log, service=None):
     try:
-        # nginx has a special format, we only need the timestamp for now
-        # TODO: add the method, path, and status code to the log
+        # nginx has a special format
         if service == "nginx":
-            timestamp = extract_timestamp_from_nginx_log(log)
-            if timestamp is None:
-                raise RuntimeError("Error getting timestamp from nginx log")
-            log_entry = {
-                "timestamp": int(timestamp.timestamp()),
-                "message": log.strip(" \n"),
-            }
+            log_entry = parse_nginx_log(log)
             return log_entry
 
+        # upgrade is a shell script without any special formatting
         if service == "upgrade":
             log_entry = {
                 "timestamp": int(datetime.now().timestamp()),
