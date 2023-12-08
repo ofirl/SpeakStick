f1e5adbaeeaf74abbe5e64d81b696209c6e7cb60
Ofir Levi <ofirl24@gmail.com>
2023-12-08 18:40:53 +0200

    fix

--- stick-controller/config.py
+++ stick-controller/config.py
@@ -7,6 +7,10 @@ def read_configs_from_db():
     for config in configsList:
         configs[config["key"]] = config["value"]
 
+    configsList = get_configs(None, 0)
+    for config in configsList:
+        configs[config["key"]] = config["value"]
+
     return configs
 
 
